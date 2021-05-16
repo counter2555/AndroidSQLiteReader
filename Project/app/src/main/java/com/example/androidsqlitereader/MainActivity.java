e58c22a9ae0a0887719e11863439017631726a65
Benedikt Limbacher <github@rattmiral.win>
2021-05-16 18:13:22 +0200

    Initial Commit

---
+++ Project/app/src/main/java/com/example/androidsqlitereader/MainActivity.java
@@ -0,0 +1,132 @@
+package com.example.androidsqlitereader;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.androidsqlitereader.ui.main.SectionsPagerAdapter;
+import com.example.androidsqlitereader.databinding.ActivityMainBinding;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import android.net.Uri;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import android.widget.ListView;
+
+import android.database.sqlite.SQLiteDatabase;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private TextView database_name_label;
+    private  ListView table_list_view;
+    private FloatingActionButton add_button;
+
+    private Overview OverviewFragment;
+
+    private SQLiteDatabase current_db = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Select .db File
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = binding.viewPager;
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = binding.tabs;
+        tabs.setupWithViewPager(viewPager);
+
+
+
+        //initialize UI elemens
+        OverviewFragment = (Overview)sectionsPagerAdapter.GetFragment(0);
+
+        add_button = (FloatingActionButton)findViewById(R.id.opendb);
+        add_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                OpenFileDialog();
+            }
+        });
+        database_name_label = (TextView)findViewById(R.id.db_name_label);
+    }
+
+    //Select .db File
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == 123 && resultCode == RESULT_OK)
+        {
+            Uri selectedfile = data.getData();
+            String filename = GetFileNameFromUri(selectedfile);
+            this.database_name_label.setText(selectedfile.getPath());
+            //this.OpenDB(selectedfile);
+        }
+    }
+
+    //Gets File Name from Uri as String
+    private static String GetFileNameFromUri(Uri url)
+    {
+        File file = new File(url.getPath());
+        return  file.getName();
+    }
+
+    private static String GetPath(Uri url)
+    {
+        File file = new File(url.getPath());
+        return  file.getPath();
+    }
+
+    private void OpenFileDialog()
+    {
+        Intent intent = new Intent().setType("*/*").setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select a file"), 123);
+    }
+
+    private void OpenDB(Uri dbfile)
+    {
+        this.current_db = SQLiteDatabase.openDatabase(GetPath(dbfile), null, SQLiteDatabase.OPEN_READONLY);
+    }
+
+    private void UpdateTables()
+    {
+        if(!Objects.isNull(current_db)) {
+            this.OverviewFragment.ClearItems();
+            Cursor c = current_db.rawQuery("SELECT name FROM sqlite_master WHERE type='table'", null);
+
+            if(c.moveToFirst())
+            {
+                while (!c.isAfterLast())
+                {
+                    String name = c.getString(0);
+                    this.OverviewFragment.AddItem(name);
+                }
+            }
+        }
+
+    }
+
+
+}
---
+++ Project/app/src/main/java/com/example/androidsqlitereader/ui/main/SectionsPagerAdapter.java
@@ -0,0 +1,73 @@
+package com.example.androidsqlitereader.ui.main;
+
+import android.content.Context;
+import android.provider.ContactsContract;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.StringRes;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import com.example.androidsqlitereader.DataFragment;
+import com.example.androidsqlitereader.Overview;
+import com.example.androidsqlitereader.QueryFragment;
+import com.example.androidsqlitereader.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * A [FragmentPagerAdapter] that returns a fragment corresponding to
+ * one of the sections/tabs/pages.
+ */
+public class SectionsPagerAdapter extends FragmentPagerAdapter {
+
+    @StringRes
+    private static final int[] TAB_TITLES = new int[]{R.string.tab_text_1, R.string.tab_text_2};
+    private final Context mContext;
+
+    private ArrayList<Fragment> fragments;
+
+    public SectionsPagerAdapter(Context context, FragmentManager fm) {
+        super(fm);
+        mContext = context;
+        fragments = new ArrayList<Fragment>();
+
+        fragments.add(new Overview());
+        fragments.add(new DataFragment());
+        fragments.add(new QueryFragment());
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return fragments.get(position);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch(position)
+        {
+            case 0:
+                return "Overview";
+            case 1:
+                return "Details";
+            case 2:
+                return "SQL Query";
+            default:
+                return "Undefined";
+        }
+    }
+
+    @Override
+    public int getCount() {
+        // Show 2 total pages.
+        return 3;
+    }
+
+    public Fragment GetFragment(int id)
+    {
+        return fragments.get(id);
+    }
+}
